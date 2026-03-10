5c8ab380d7bfd82a8278817841f7fb57c75ea826
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2026-03-10 21:36:15 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -16,6 +16,10 @@ func main() {
 			panic(err)
 		}
 		parts := strings.Split(strings.TrimSpace(line), " ")
+		builtins := make(map[string]struct{})
+		builtins["exit"] = struct{}{}
+		builtins["echo"] = struct{}{}
+		builtins["type"] = struct{}{}
 		command := parts[0]
 		args := parts[1:]
 
@@ -24,6 +28,14 @@ func main() {
 			os.Exit(0)
 		case "echo":
 			fmt.Println(strings.Join(args, " "))
+		case "type":
+			for _, arg := range args {
+				if _, ok := builtins[arg]; ok {
+					fmt.Printf("%s is a shell builtin\n", arg)
+				} else {
+					fmt.Printf("%s: not found\n", arg)
+				}
+			}
 		default:
 			fmt.Printf("%s: command not found\n", command)
 		}
