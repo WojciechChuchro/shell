45306366b88c97ee885d9afcc24484512d4d9fd2
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2026-03-23 18:19:03 +0100

    Refactor lexer to return pointers for tokens and improve token reading logic

--- app/lexer/lexer.go
+++ app/lexer/lexer.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"unicode"
 )
 
 type Lexer struct {
@@ -13,20 +14,30 @@ type Lexer struct {
 	prev   rune
 }
 
-func (l *Lexer) NextToken() (Token, error) {
+func (l *Lexer) NextToken() (*Token, error) {
 	l.skipWhiteSpace()
+	if l.cur == 0 {
+		return nil, io.EOF
+	}
 
 	switch l.cur {
 	default:
-		l.readWord()
-		return Token{}, io.EOF
+		word := l.readWord()
+		if word == "" {
+			return nil, io.EOF
+		}
+		return NewToken(Word, word), nil
 	}
-
 }
 
-func (l *Lexer) NextRune() {
+func (l *Lexer) advance() {
 	r, _, err := l.Reader.ReadRune()
 	if err != nil {
+		if err == io.EOF {
+			l.prev = l.cur
+			l.cur = 0
+			return
+		}
 		panic(fmt.Sprintf("error reading rune: %v", err))
 	}
 
@@ -34,20 +45,44 @@ func (l *Lexer) NextRune() {
 	l.cur = r
 }
 
-func (l *Lexer) readWord() {
-
+func (l *Lexer) readWord() string {
+	if l.cur == '\'' {
+		l.advance()
+		word := ""
+		for l.cur != 0 && l.cur != '\'' {
+			word += string(l.cur)
+			l.advance()
+		}
+		if l.cur == '\'' {
+			l.advance()
+		}
+		return word
+	}
+	word := ""
+	for l.cur != 0 && !unicode.IsSpace(l.cur) {
+		word += string(l.cur)
+		l.advance()
+	}
+	return word
 }
 
 func (l *Lexer) skipWhiteSpace() {
-	for l.cur != ' ' {
-		l.NextRune()
+	for unicode.IsSpace(l.cur) && l.cur != 0 {
+		l.advance()
 	}
 }
 
 func NewLexer(reader bufio.Reader) *Lexer {
 	r, _, err := reader.ReadRune()
 	if err != nil {
+		if err == io.EOF {
+			return &Lexer{Reader: reader, Tokens: []Token{}, cur: 0}
+		}
 		panic(fmt.Sprintf("error reading first rune: %v", err))
 	}
 	return &Lexer{Reader: reader, Tokens: []Token{}, cur: r}
+}
+
+func isAlphaNumeric(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsDigit(r)
 }
